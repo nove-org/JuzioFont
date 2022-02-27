377af68d179b0ca4755d02d42d25b4dabd6c14e8
wnm210 <49320100+wnm210@users.noreply.github.com>
2022-02-27 17:50:59 +0100

    init.m

---
+++ other/init.m
@@ -0,0 +1,19 @@
+#import <Foundation/Foundation.h>
+
+@interface SampleClass:NSObject
+- (void)sampleMethod;
+@end
+
+@implementation SampleClass
+
+- (void)sampleMethod {
+   NSLog(@"Hello, World! \n");
+}
+
+@end
+
+int main() {
+   SampleClass *sampleClass = [[SampleClass alloc]init];
+   [sampleClass sampleMethod];
+   return 0;
+}
